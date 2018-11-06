71486444fa9aaee2c1c3a2f00b753b45e4b8f999
Frederique Mittelstaedt <frederique@automorph.com>
2018-11-06 18:02:26 +0000

    Correct spelling error in Image component

--- packages/react/src/components/media/Image.ts
+++ packages/react/src/components/media/Image.ts
@@ -6,6 +6,7 @@
 
 import * as React from "react";
 import setDisplayName from "recompose/setDisplayName";
+import withProps from "recompose/withProps";
 import { IdMonad, PropsType, RCT } from "../../util";
 import { Box, CssProps } from "../base/Box";
 
@@ -13,8 +14,20 @@ import { Box, CssProps } from "../base/Box";
 
 export interface ImageProps extends CssProps, PropsType<"img"> {}
 
-export const ImageProps = IdMonad.of((props: ImageProps) =>
+export const Image = IdMonad.of((props: ImageProps) =>
   React.createElement(Box, { as: "img", ...props }),
 )
+  .map(withProps<ImageProps, ImageProps>(({css, ...props}) => ({
+    css: [
+      {
+        display: "block",
+        maxWidth: "100%",
+        maxHeight: "100%",
+        width: "auto",
+        height: "auto"
+      },
+      css
+    ]
+  }))));
   .map(setDisplayName("Image"))
   .flatten() as RCT<ImageProps>;
