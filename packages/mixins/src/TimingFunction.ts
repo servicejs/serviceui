e971232541e2e2b51954c0b3996603a89b096176
Frederique Mittelstaedt <frederique@automorph.com>
2019-01-07 22:50:23 +0000

    Replace const enums with namespaces

--- packages/mixins/src/TimingFunction.ts
+++ packages/mixins/src/TimingFunction.ts
@@ -4,22 +4,18 @@
 
 // tslint:disable-next-line:no-namespace
 namespace TimingFunction {
-  export const enum Static {
-    ease = "ease",
-    easeIn = "ease-in",
-    easeOut = "ease-out",
-    easeInOut = "ease-in-out",
-    stepStart = "step-start",
-    stepEnd = "step-end",
-  }
+  export const ease = "ease";
+  export const easeIn = "ease-in";
+  export const easeOut = "ease-out";
+  export const easeInOut = "ease-in-out";
+  export const stepStart = "step-start";
+  export const stepEnd = "step-end";
 
-  export namespace Dynamic {
-    export const cubicBezier = (fourNumbers: [number, number, number, number]) =>
-      `cubic-bezier(${fourNumbers.map((n) => n.toString()).join(", ")})`;
-    export const stepsStartN = (n: number) => `steps(${n}, start)`;
-    export const stepsEndN = (n: number) => `steps(${n}, end)`;
-    export const frames = (n: number) => `frames(${n})`;
-  }
+  export const cubicBezier = (fourNumbers: [number, number, number, number]) =>
+    `cubic-bezier(${fourNumbers.map((n) => n.toString()).join(", ")})`;
+  export const stepsStartN = (n: number) => `steps(${n}, start)`;
+  export const stepsEndN = (n: number) => `steps(${n}, end)`;
+  export const frames = (n: number) => `frames(${n})`;
 }
 
 export default TimingFunction;
